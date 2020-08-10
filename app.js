9e480f5914580c12d1d21f05993e982417dec30d
[disglair] <[deepjchetia007@gmail.com]>
2020-08-10 14:41:33 +0530

    just a commit

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
-var multer  = require('multer');
+var multer = require('multer');
 var Image = require('./models/image.js')
-const {randomNumber} = require('./helpers/random.js');
+const {
+  randomNumber
+} = require('./helpers/random.js');
 const path = require('path');
 const fs = require('fs');
 
@@ -10,30 +12,35 @@ const port = 3000;
 
 // Mongoose config
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/imageApp', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://127.0.0.1:27017/imageApp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function () {
   console.log('DB connected');
 });
 // Multer storage
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/tmp/my-uploads')
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(jpeg|jpg|png)$/)) {
-            return cb(new Error('Please upload a image file'));
-        }
-        cb(undefined, true);
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now())
+  destination: function (req, file, cb) {
+    cb(null, './public/tmp/my-uploads')
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpeg|jpg|png)$/)) {
+      return cb(new Error('Please upload a image file'));
     }
-  });
-   
-var upload = multer({ storage: storage });
+    cb(undefined, true);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+});
+
+var upload = multer({
+  storage: storage
+});
 
 // Middlewares
 app.set('view engine', 'ejs');
@@ -43,12 +50,16 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   Image.find({})
     .then(result => {
-      res.render('index.ejs', { image:result })
+      res.render('index.ejs', {
+        image: result
+      })
     })
-    .catch(err => {throw new Error})
+    .catch(err => {
+      throw new Error
+    })
 })
 
-app.get('/about', (req, res)=> {
+app.get('/about', (req, res) => {
   res.render('about.ejs');
 })
 
@@ -57,12 +68,13 @@ app.get('/upload', (req, res) => {
 });
 
 app.post('/upload', upload.single('photo'), (req, res, next) => {
-    const saveimage = () => {
-      const tempImagePath = req.file.path;
-      const imageUrl = randomNumber();
-      const ext = path.extname(req.file.originalname).toLowerCase();
-      const targetPath = path.resolve(`public/tmp/my-uploads/${imageUrl}${ext}`);
+  const saveimage = () => {
+    const tempImagePath = req.file.path;
+    const imageUrl = randomNumber();
+    const ext = path.extname(req.file.originalname).toLowerCase();
+    const targetPath = path.resolve(`public/tmp/my-uploads/${imageUrl}${ext}`);
 
+    if (ext == '.png' || ext == '.svg' || ext == '.jpg' || ext == '.jpeg') {
       fs.rename(tempImagePath, targetPath, (err) => {
         if (err) throw err;
         console.log('Rename complete!');
@@ -75,9 +87,10 @@ app.post('/upload', upload.single('photo'), (req, res, next) => {
 
       image.save();
     }
+  }
 
-    saveimage();
-    res.redirect('/');
+  saveimage();
+  res.redirect('/');
 })
 
 app.listen(port, () => {
--- models/image.js
+++ models/image.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const imageSchema = new mongoose.Schema({
     caption : {
         type: String,
-        // required: true
+        required: true
     },
 
     path : {
